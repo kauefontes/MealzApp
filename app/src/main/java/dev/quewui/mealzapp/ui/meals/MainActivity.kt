92edb5cf16fea418df5e98fea46d66543d11a36b
Kaue Pereira <kauefontes@outlook.com>
2022-10-06 23:29:55 -0400

    Lazy load list of Categories from remote API(Free Meal API)

--- app/src/main/java/dev/quewui/mealzapp/ui/meals/MainActivity.kt
+++ app/src/main/java/dev/quewui/mealzapp/ui/meals/MainActivity.kt
@@ -3,11 +3,17 @@ package dev.quewui.mealzapp.ui.meals
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import dev.quewui.mealzapp.model.response.CategoryResponse
 import dev.quewui.mealzapp.ui.theme.MealzAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,12 +30,15 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CategoriesScreen() {
     val viewModel: MealsCategoriesViewModel = viewModel()
-    val categories = viewModel.getMeals()
-    Row {
-        if (categories != null) {
-            for (category in categories) {
-                Text(text = category.name)
-            }
+    val categoriesResponse: MutableState<List<CategoryResponse>> = remember {
+        mutableStateOf(emptyList())
+    }
+    viewModel.getMeals { response ->
+        categoriesResponse.value = response?.categories.orEmpty()
+    }
+    LazyColumn {
+        items(categoriesResponse.value) { category ->
+            Text(text = category.name, style = MaterialTheme.typography.h2)
         }
     }
 }
--- app/src/main/java/dev/quewui/mealzapp/ui/meals/MealsCategoriesViewModel.kt
+++ app/src/main/java/dev/quewui/mealzapp/ui/meals/MealsCategoriesViewModel.kt
@@ -2,11 +2,13 @@ package dev.quewui.mealzapp.ui.meals
 
 import androidx.lifecycle.ViewModel
 import dev.quewui.mealzapp.model.MealsRepository
-import dev.quewui.mealzapp.model.response.CategoryResponse
+import dev.quewui.mealzapp.model.response.MealsCategoriesResponse
 
 class MealsCategoriesViewModel(private val repository: MealsRepository = MealsRepository()) :
     ViewModel() {
-        fun getMeals(): List<CategoryResponse>? {
-            return repository.getMeals()?.categories
+    fun getMeals(successCallback: (response: MealsCategoriesResponse?) -> Unit) {
+        return repository.getMeals { response ->
+            successCallback(response)
         }
     }
+}
